aef796cacd440131d0929329b309adb4feffff34
Marta Taulet <mta@odoo.com>
2021-04-05 18:06:10 +0000

    [ADD] Basic Module

---
+++ art_gallery/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Galeria de Arte",
+
+    'summary': """
+        Mostrar Obras de Arte""",
+
+    'description': """
+        Mostrar Obras de Arte
+    """,
+
+    'author': "Odoo",
+    'website': "http://www.odoo.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Training',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+}
